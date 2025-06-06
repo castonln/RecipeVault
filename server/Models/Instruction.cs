c08897ff4975fffc8aa7971d4b55304907a0abbb
Eli Rindler <elirindler@icloud.com>
2025-06-06 10:03:50 -0400

    Updated db model

---
+++ server/Models/Instruction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace server.Models;
+
+public partial class Instruction
+{
+    public Guid Id { get; set; }
+
+    public decimal InstructionNumber { get; set; }
+
+    public Guid RecipeId { get; set; }
+
+    public string Description { get; set; } = null!;
+
+    public virtual Recipe Recipe { get; set; } = null!;
+}
---
+++ server/Models/Recipe.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace server.Models;
+
+public partial class Recipe
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public decimal? Servings { get; set; }
+
+    public decimal? ServingSize { get; set; }
+
+    public decimal? PrepTime { get; set; }
+
+    public decimal? CookTime { get; set; }
+
+    public Guid CreatedBy { get; set; }
+
+    public virtual User CreatedByNavigation { get; set; } = null!;
+
+    public virtual ICollection<Instruction> Instructions { get; set; } = new List<Instruction>();
+
+    public virtual ICollection<RecipeIngredient> RecipeIngredients { get; set; } = new List<RecipeIngredient>();
+}
