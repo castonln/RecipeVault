b719d8774f6aa7a2e1b04cc2f190bc11ab59101b
Logan <castonln@mail.uc.edu>
2025-06-10 18:51:35 -0400

    added sign in, sign up, and recipe book fillers

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -1,4 +1,5 @@
 import { Stack, Paper, Box, TextField, Typography, Button } from "@mui/material";
+import { NavLink } from "react-router";
 
 const SignIn = () => {
     const handleSubmit = () => {
@@ -20,10 +21,20 @@ const SignIn = () => {
                         sx={{
                             textAlign: "center",
                             marginBottom: "30px"
-                        }}>Sign In</Typography>
+                        }}>
+                            Sign In
+                        </Typography>
+
                     <TextField placeholder="Username" fullWidth required autoFocus sx={{ marginBottom: "20px" }} />
                     <TextField placeholder="Password" fullWidth required type="password" sx={{ marginBottom: "20px" }} />
                     <Button type="submit" variant="contained" fullWidth>Sign In</Button>
+                    
+                    <Typography sx={{
+                        marginTop: "20px",
+                        textAlign: "center",
+                    }}>
+                        Don't have an account? <NavLink to={"/signup"}>Sign up</NavLink>
+                    </Typography>
                 </Box>
             </Paper>
         </Stack>
---
+++ client/src/components/SignUp.jsx
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material";
+
+const SignUp = () => {
+    return (
+    <Box>Sign Up</Box>
+    );
+}
+
+export default SignUp;
