ded1c702a284ac053b2f47b7af671c7aeeeb815a
Massimo <lanciamg@mail.uc.edu>
2025-06-16 18:16:39 -0400

    Added recipe book change

--- client/src/components/RecipeBook.jsx
+++ client/src/components/RecipeBook.jsx
@@ -1,9 +1,58 @@
-import { Box } from "@mui/material";
+import { Box, Card, CardContent, Grid, Typography } from "@mui/material";
 
 const RecipeBook = () => {
-    return ( 
-        <Box>Recipe Book.</Box>
-     );
+    return (
+        <Grid container spacing={2}>
+            <Grid size={3}>
+                <Card sx={{height: '150px'}}>
+                    <CardContent>
+                        <Typography Variant="h5" component="div">
+                            Title
+                        </Typography>
+                        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                            Recipe description...
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Grid>
+            <Grid size={3}>
+                <Card sx={{height: '150px'}}>
+                    <CardContent>
+                        <Typography Variant="h5" component="div">
+                            Title
+                        </Typography>
+                        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                            Recipe description...
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Grid>
+            <Grid size={3}>
+                <Card sx={{height: '150px'}} >
+                    <CardContent>
+                        <Typography Variant="h5" component="div">
+                            Title
+                        </Typography>
+                        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                            Recipe description...
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Grid>
+            <Grid size={3}>
+                <Card sx={{height: '150px'}}>
+                    <CardContent>
+                        <Typography Variant="h5" component="div">
+                            Title
+                        </Typography>
+                        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                            Recipe description...
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Grid>
+        </Grid>
+    );
 }
- 
+
 export default RecipeBook;
