ca2b9ca6aa66d5d71102f097dfc430a8269191db
Eli Rindler <elirindler@icloud.com>
2025-07-01 20:08:12 -0400

    Calculate metadata

--- server/Data/DTO/IngredientDTO.cs
+++ server/Data/DTO/IngredientDTO.cs
@@ -10,5 +10,23 @@ namespace server.Data.DTO
 
         [JsonPropertyName("fdaId")]
         public decimal? FdaId { get; set; }
+
+        [JsonPropertyName("servingSize")]
+        public decimal? ServingSize { get; set; }
+
+        [JsonPropertyName("unit")]
+        public string? Unit { get; set; }
+
+        [JsonPropertyName("calories")]
+        public decimal? Calories { get; set; }
+
+        [JsonPropertyName("carbs")]
+        public decimal? Carbs { get; set; }
+
+        [JsonPropertyName("protein")]
+        public decimal? Protein { get; set; }
+
+        [JsonPropertyName("fats")]
+        public decimal? Fats { get; set; }
     }
 }
